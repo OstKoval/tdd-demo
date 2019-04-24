bdaf53f3e3b0b554053b8a6846c048ff1623fd0a
ostap.kovalchuk <ostap.kovalchuk@sombrainc.com>
2019-04-24 13:29:01 +0300

    initial commit

---
+++ TddDemo/src/test/java/com/tdd/demo/unit/merchant/MerchantTest.java
@@ -0,0 +1,14 @@
+package com.tdd.demo.unit.merchant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.Silent.class)
+public class MerchantTest {
+
+    @Test
+    public void addMerchantTest(){
+
+    }
+}
